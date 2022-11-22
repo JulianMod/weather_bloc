1b631d91d31f0c9728fb344b499d266f004bde8b
JuliaMod <julmodlinski@gmail.com>
2022-11-22 12:55:34 +0100

    add app

---
+++ lib/app.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_bloc/theme/cubit/theme_cubit.dart';
+import 'package:weather_repository/weather_repository.dart';
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({Key? key, required WeatherRepository weatherRepository}) : super(key: key);
+
+  final WeatherRepository _weatherRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider.value(
+      value: _weatherRepository,
+      child: BlocProvider(
+        create: (_) => ThemeCubit(),
+        child: const WeatherAppView(),
+      ),
+    );
+  }
+}
+
+class WeatherAppView extends StatelessWidget {
+  const WeatherAppView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return BlocBuilder<ThemeCubit, Color>(
+        builder: (context, color) {
+          return MaterialApp(
+            theme: ThemeData(
+                primaryColor: color,
+                textTheme: GoogleFonts.rajdhaniTextTheme(),
+                appBarTheme: AppBarTheme(
+                    titleTextStyle: GoogleFonts.rajdhaniTextTheme(textTheme)
+                        .apply(bodyColor: Colors.white)
+                        .headline6
+                )
+            ),
+            home: const WeatherPage(),
+          );
+        });
+  }
+}
